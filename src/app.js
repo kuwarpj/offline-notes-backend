a91dc36b324f4f1a525f58f2037fb30458fc4d4b
kuwar jha <kuwarjha.cse@gmail.com>
2025-05-22 18:16:11 +0530

    added db connection and global error catcher

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
+
+app.use((err, req, res, next) => {
+
+  const statusCode = err.statusCode || 500;
+
+  const response = {
+    success: err.success,
+    message: err.message || "Internal Server Error",
+    ...(err.data && { data: err.data }),
+    ...(Array.isArray(err.errors) && err.errors.length > 0 && { errors: err.errors }),
+  };
+
+  res.status(statusCode).json(response);
+});
+
+export default app;
---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${process.env.DB_NAME}`
+    );
+    console.log("Mongodb connected", connection.connection.host);
+  } catch (error) {
+    console.log("MongoDB Error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB
